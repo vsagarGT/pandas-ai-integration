148aefbbc4e82329213f1a5bc8359159902e98ab
Ishwarya Sivakumar <ish23.siva@gmail.com>
2023-10-12 16:55:07 -0400

    updated the function to remove langchain.

--- chat_runner.py
+++ chat_runner.py
@@ -6,7 +6,7 @@ cursor = evadb.connect().cursor()
 print("Connected to EvaDB")
 
 create_function_query = f"""CREATE FUNCTION IF NOT EXISTS ChatWithPandas
-            IMPL  './functions/semantic_cache.py';
+            IMPL  './functions/chat_with_df.py';
             """
 cursor.query("DROP FUNCTION IF EXISTS ChatWithPandas;").execute()
 cursor.query(create_function_query).execute()
@@ -29,20 +29,33 @@ gear FLOAT(64,64),
 carb FLOAT(64,64)
 );
 """
-load_data_query = f""" LOAD CSV 'data/cars.csv' INTO CARSDATA;
-"""
+
+
+columns = ['id', 'name', 'mpg', 'cyl', 'disp', 'hp', 'drat', 'wt', 'qsec', 'vs', 'am', 'gear', 'carb']
+
+
+load_data_query = f""" LOAD CSV 'data/cars.csv' INTO CARSDATA;""" 
 
 cursor.query(create_table_query).execute()
 cursor.query(load_data_query).execute()
+
 print("loaded data")
 
-chat_query1 = f""" SELECT ChatWithPandas('what is the mean of the gear column',gear, name) FROM CARSDATA;
+chat_query1 = f""" SELECT ChatWithPandas('query', 'what is the mean of the gear column',gear, name) FROM CARSDATA;
 """
-
 result1 = cursor.query(chat_query1).execute()
 print(result1)
 
-chat_query2 = f""" SELECT ChatWithPandas('which car has the highest gear value',gear, name) FROM CARSDATA;
-"""
-result2 = cursor.query(chat_query2).execute()
-print(result2)
+
+# chat_query2 = f""" SELECT ChatWithPandas('plot', 'plot a bar graph of mpg vs hp',mpg, hp) FROM CARSDATA;
+# """
+# result2 = cursor.query(chat_query2).execute()
+# print(result2)
+
+
+# chat_query3 = f""" SELECT ChatWithPandas('manipulation', 'make all the names in the NAME column small', name) FROM CARSDATA;
+# """
+# result3 = cursor.query(chat_query3).execute()
+# print(result3)
+
+
