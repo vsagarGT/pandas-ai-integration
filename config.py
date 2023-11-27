064af10815861132c60755da1ff2f974e4c24d28
Vikram Sagar <vikram@MacBook-Pro-2.local>
2023-11-27 12:33:33 -0500

    Adjusted config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     def __init__(self) -> None:
-        self.open_ai_key = "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"
+        self.open_ai_key = ""
 
     def get_open_ai_key(self):
         return self.open_ai_key
